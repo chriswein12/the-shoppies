c6dc38bdfecf73b3c1b2c074d670d3e706fc4b9c
Chris Wein <chriswein12@yahoo.com>
2021-05-10 02:34:20 -0500

    Nominations working correctly

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const movieSearchInputEl = document.querySelector("#search-input");
 const movieSearchFormEl = document.querySelector("#movie-search-form");
 const movieSearchResultsEl = document.querySelector("#search-results");
+const nominationsListEl = document.querySelector("#nomination-list");
 
 const nominations = [];
 
@@ -9,6 +10,7 @@ const movieSearch = function(event) {
 
     let movieName = movieSearchInputEl.value.trim();
 
+    // fetch request and error handling from the OMDB database.
     if (movieName) {
         const apiUrl = `http://www.omdbapi.com/?s=${movieName}&apikey=c09abee9`;
 
@@ -30,7 +32,7 @@ const movieSearch = function(event) {
 }
 
 function displaySearchResults(movieResults) {
-    console.log(movieResults);
+
     movieSearchResultsEl.textContent = "";
 
     if (movieResults.Response === "False") {
@@ -38,8 +40,7 @@ function displaySearchResults(movieResults) {
         return;
     } 
 
-    // const resultsListEl = document.createElement("ul");
-
+    // displays the results of the search.
     for (let i = 0; i < movieResults.Search.length; i++) {
         const movieName = movieResults.Search[i].Title;
         const movieYear = movieResults.Search[i].Year;
@@ -59,6 +60,7 @@ function displaySearchResults(movieResults) {
         singleResultEl.appendChild(singleResultTextEl);
         movieSearchResultsEl.appendChild(singleResultEl);
 
+        // each time the search results load, a check is done to see if a result has already been nominated.
         for (let i = 0; i < nominations.length; i++) {
             if (movieInfo === nominations[i]) {
                 nominateBtnEl.setAttribute("disabled", "")
@@ -67,25 +69,70 @@ function displaySearchResults(movieResults) {
             }
         }
     }
+
 }
 
 const addNomination = function (event) {
+
+    if (nominations.length >= 5) {
+        alert("You have already nominated 5 movies");
+        return;
+    }
+
     const targetEl = event.target;
     const targetText = targetEl.previousSibling.textContent;
 
+    // disable button after click.
     targetEl.setAttribute("disabled", "");
 
     nominations.push(targetText);
 
-    displayNominations(targetText);        
-}
-
-function displayNominations(movieNominations) {
-
+    displayNominations();        
 }
 
 
+function displayNominations() {
 
+    nominationsListEl.textContent = "";
+
+    for (let i = 0; i < nominations.length; i++) {
+        
+        const singleNomineeEl = document.createElement("li");
+        const singleNomineeTextEl = document.createElement("p");
+        const removeNomineeBtnEl = document.createElement("button");
+
+        singleNomineeTextEl.textContent = nominations[i];
+        removeNomineeBtnEl.classList = "btn nominate-btn btn-secondary";
+        removeNomineeBtnEl.textContent = "Remove";
+        removeNomineeBtnEl.setAttribute("id", "nomineeId-" + i);
+        removeNomineeBtnEl.addEventListener("click", removeNomination);
+
+        singleNomineeTextEl.append(removeNomineeBtnEl);
+        singleNomineeEl.appendChild(singleNomineeTextEl);
+        nominationsListEl.appendChild(singleNomineeEl);
+    }
+}
+
+const removeNomination = function (event) {
+
+    const targetElId = event.target.getAttribute("id").split("-");
+    const id = targetElId[1];
+
+    // Removes the "disabled" attribute from the button in the search list if the movie is still showing in the results
+    const disabledButtons = movieSearchResultsEl.querySelectorAll("button[disabled]");
+    
+    for (let i = 0; i < disabledButtons.length; i++) {
+        const text = disabledButtons[i].previousSibling.textContent;
+        
+        if (text === nominations[id]) {
+            disabledButtons[i].removeAttribute("disabled");
+        }
+    }
+
+    nominations.splice(id, 1);
+
+    displayNominations();
+}
 
 
 movieSearchFormEl.addEventListener("submit", movieSearch);
